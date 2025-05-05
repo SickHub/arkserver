17f223eaa0a037906cfc16b8a598341c27fccddc
Kjeld Schouten <info@kjeldschouten.nl>
2025-05-05 22:06:21 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -119,7 +119,6 @@ if [ ! -f /ark/config/crontab ]; then
 # |  .------------- hour (0 - 23)
 # |  |  .---------- day of month (1 - 31)
 # |  |  |  .------- month (1 - 12) OR jan,feb,mar,apr ...
-# |  |  |  |  .---- day of week (0 - 6) (Sunday=0 or 7)
 # |  |  |  |  .---- day of week (0 - 6) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat
 # |  |  |  |  |
 # *  *  *  *  * user-name  command to be executed
